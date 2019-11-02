ff9ab4779efde2f1220cf0580cf7788efef9adb5
Markus Brueckner <namenlos@geekbetrieb.de>
2019-11-02 19:57:53 +0100

    - add toolbar in every mainview

--- packages/frontend/src/components/Inbox.tsx
+++ packages/frontend/src/components/Inbox.tsx
@@ -4,13 +4,27 @@ import React from 'react';
 import { GlobalState } from '../model/GlobalState';
 import { TaskList } from './TaskList';
 import { inboxFilter } from '../util/Filter';
+import { AppBar, Toolbar, makeStyles, createStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        appBar: {
+            top: 'auto',
+            bottom: 0
+        }
+    })
+);
 
 export function Inbox(props: IDispatchReceiver) {
+    const classes = useStyles();
     return (
         <GlobalState.Consumer>
             {state => (
                 <Fragment>
                     <TaskList tasks={inboxFilter(state.tasks)} />
+                    <AppBar className={classes.appBar}>
+                        <Toolbar></Toolbar>
+                    </AppBar>
                 </Fragment>
             )}
         </GlobalState.Consumer>
--- packages/frontend/src/components/SomedayMaybe.tsx
+++ packages/frontend/src/components/SomedayMaybe.tsx
@@ -4,13 +4,29 @@ import React from 'react';
 import { GlobalState } from '../model/GlobalState';
 import { TaskList } from './TaskList';
 import { somedayMaybeFilter } from '../util/Filter';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles, createStyles } from '@material-ui/styles';
+import { Toolbar } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        appBar: {
+            top: 'auto',
+            bottom: 0
+        }
+    })
+);
 
 export function SomedayMaybe(props: IDispatchReceiver) {
+    const classes = useStyles();
     return (
         <GlobalState.Consumer>
             {state => (
                 <Fragment>
                     <TaskList tasks={somedayMaybeFilter(state.tasks)} />
+                    <AppBar className={classes.appBar}>
+                        <Toolbar></Toolbar>
+                    </AppBar>
                 </Fragment>
             )}
         </GlobalState.Consumer>
