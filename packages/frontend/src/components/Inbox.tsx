117bb0c40c79fd0e2d4f717bbecb80e0bb352bfe
Markus Brueckner <namenlos@geekbetrieb.de>
2019-11-09 13:43:19 +0100

    - include config button on every tab

--- packages/frontend/src/components/Inbox.tsx
+++ packages/frontend/src/components/Inbox.tsx
@@ -3,26 +3,45 @@ import React from 'react';
 import { GlobalState } from '../model/GlobalState';
 import { TaskList } from './TaskList';
 import { inboxFilter } from '../util/Filter';
-import { AppBar, Toolbar, makeStyles, createStyles } from '@material-ui/core';
+import {
+    AppBar,
+    Toolbar,
+    makeStyles,
+    createStyles,
+    Button
+} from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles(() =>
     createStyles({
         appBar: {
             top: 'auto',
             bottom: 0
+        },
+        settings: {
+            marginLeft: 'auto',
+            marginRight: 0
         }
     })
 );
 
 export function Inbox() {
     const classes = useStyles();
+    const history = useHistory();
     return (
         <GlobalState.Consumer>
             {state => (
                 <Fragment>
                     <TaskList tasks={inboxFilter(state.config, state.tasks)} />
                     <AppBar className={classes.appBar}>
-                        <Toolbar></Toolbar>
+                        <Toolbar>
+                            <Button
+                                className={classes.settings}
+                                onClick={() => history.push('/config')}>
+                                <SettingsIcon className={classes.settings} />
+                            </Button>
+                        </Toolbar>
                     </AppBar>
                 </Fragment>
             )}
--- packages/frontend/src/components/SomedayMaybe.tsx
+++ packages/frontend/src/components/SomedayMaybe.tsx
@@ -5,26 +5,39 @@ import { TaskList } from './TaskList';
 import { somedayMaybeFilter } from '../util/Filter';
 import AppBar from '@material-ui/core/AppBar';
 import { makeStyles, createStyles } from '@material-ui/styles';
-import { Toolbar } from '@material-ui/core';
+import { Toolbar, Button } from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles(() =>
     createStyles({
         appBar: {
             top: 'auto',
             bottom: 0
+        },
+        settings: {
+            marginLeft: 'auto',
+            marginRight: 0
         }
     })
 );
 
 export function SomedayMaybe() {
     const classes = useStyles();
+    const history = useHistory();
     return (
         <GlobalState.Consumer>
             {state => (
                 <Fragment>
                     <TaskList tasks={somedayMaybeFilter(state.tasks)} />
                     <AppBar className={classes.appBar}>
-                        <Toolbar></Toolbar>
+                        <Toolbar>
+                            <Button
+                                className={classes.settings}
+                                onClick={() => history.push('/config')}>
+                                <SettingsIcon className={classes.settings} />
+                            </Button>
+                        </Toolbar>
                     </AppBar>
                 </Fragment>
             )}
